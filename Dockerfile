965ffd2b38b8084f50eef973ae6ecc78845b4275
Andrew Elliot <andrwmorph@gmail.com>
2019-04-29 21:27:25 -0400

    Running as non-root for vlc

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@ FROM python:3-alpine
 
 WORKDIR /usr/src/app
 
-RUN apk add --no-cache git zlib-dev jpeg-dev gcc musl-dev vlc opus-dev && mkdir /data
+RUN apk add --no-cache git zlib-dev jpeg-dev gcc musl-dev vlc opus-dev && chown 910:910 . && mkdir /data && chown 910:910 /data
 
-RUN git clone --recurse-submodules https://github.com/DuckBoss/JJMumbleBot . && pip3 install --no-cache-dir -r requirements.txt
+RUN git clone --recurse-submodules https://github.com/DuckBoss/JJMumbleBot . && pip3 install --no-cache-dir -r requirements.txt 
+
+USER 910:910
 
 VOLUME /data/logs /data/media /data/cert /usr/src/app/privileges
 
--- init.sh
+++ init.sh
@@ -4,11 +4,14 @@ echo "Starting JJMumbleBot..."
 SERVER_IP=${ADDRESS:-localhost}
 PASSWORD=${PASSWORD:-password}
 PORT_NUMBER=${PORT:-64738}
+DEFAULT_CHANNEL_NAME=${CHANNEL:-default}
+USERNAME=${BOTNAME:-jjmumblebot}
 
 sed "s|SERVER_IP|$SERVER_IP|;
      s|PASSWORD|$PASSWORD|;
      s|PORT_NUMBER|$PORT_NUMBER|;
-     s|USERNAME|jjmumblebot|;
+     s|DEFAULT_CHANNEL_NAME|$DEFAULT_CHANNEL_NAME|;
+     s|USERNAME|$USERNAME|;
      s|CERT_FILE_PATH|/data/cert/cert.pem|;
      s|TEMP_IMG_DIR_PATH|/data/tmp|;
      s|PERM_MEDIA_DIR_PATH|/data/media|;
@@ -17,4 +20,4 @@ sed "s|SERVER_IP|$SERVER_IP|;
      s|LOGS_DIR_PATH|/data/logs|;" \
      templates/sample_config.ini > config.ini
 
-python3 JJMumbleBot.py
+python3 .
